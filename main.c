4da6b765d5689b6ea8d9e1a7c26c2f157d11c87f
0ldStar <brosto_man@mail.ru>
2021-05-03 20:21:01 +0700

    fix overuse of memory

--- main.c
+++ main.c
@@ -3,50 +3,63 @@
 #include <time.h>
 #include <limits.h>
 
+int Demon_sort_calc(int *arr, int begin, int end, int *left_max, int *right_min) {
+    int len = end - begin;
+    int middle = begin + len / 2;
+    int *tmp_arr;
+    while (!(tmp_arr = (int *) malloc(len * sizeof(int)))) {
+        printf("MEM FAULT\n");
+        exit(-1);
+    }
+    *left_max = INT_MIN, *right_min = INT_MAX;
+    int left_point = 0, right_point = len - 1;
+
+    if (len == 1) {
+        free(tmp_arr);
+        return -1;
+    }
+    for (int i = begin; i < middle; ++i) {
+        if (arr[i] > *left_max) *left_max = arr[i];
+    }
+    for (int i = middle; i < end; ++i) {
+        if (arr[i] < *right_min) *right_min = arr[i];
+    }
+    if (*left_max <= *right_min) {
+        free(tmp_arr);
+        return -1;
+    }
+    if (len == 2) {
+        if (arr[begin] > arr[end - 1]) {
+            int buf = arr[begin];
+            arr[begin] = arr[end - 1];
+            arr[end - 1] = buf;
+        }
+        free(tmp_arr);
+        return -1;
+    }
+    for (int i = begin; i < middle; ++i) {
+        if (arr[i] <= *right_min) tmp_arr[left_point++] = arr[i];
+        else tmp_arr[right_point--] = arr[i];
+    }
+    for (int i = middle; i < end; ++i) {
+        if (arr[i] >= *left_max) tmp_arr[right_point--] = arr[i];
+        else tmp_arr[left_point++] = arr[i];
+    }
+    for (int i = begin, k = 0; i < end; ++i, ++k) {
+        arr[i] = tmp_arr[k];
+    }
+    free(tmp_arr);
+    return 0;
+}
+
 void Demon_sort(int *arr, int begin, int end) {
     int len = end - begin;
-    int tmp_arr[len];
     int left_max = INT_MIN, right_min = INT_MAX;
     int middle = begin + len / 2;
-    int left_point, right_point;
 
     do {
-        left_max = INT_MIN, right_min = INT_MAX;
-        left_point = 0, right_point = len - 1;
+        if (Demon_sort_calc(arr, begin, end, &left_max, &right_min) == -1)return;
 
-        if (len == 1) {
-            return;
-        }
-        for (int i = begin; i < middle; ++i) {
-            if (arr[i] > left_max) left_max = arr[i];
-        }
-        for (int i = middle; i < end; ++i) {
-            if (arr[i] < right_min) right_min = arr[i];
-        }
-        if (left_max <= right_min) {
-            return;
-        }
-        if (len == 2) {
-            if (arr[begin] > arr[end - 1]) {
-                int buf = arr[begin];
-                arr[begin] = arr[end - 1];
-                arr[end - 1] = buf;
-            }
-            return;
-        }
-        for (int i = begin; i < middle; ++i) {
-            if (arr[i] <= right_min) tmp_arr[left_point++] = arr[i];
-            else tmp_arr[right_point--] = arr[i];
-        }
-        for (int i = middle; i < end; ++i) {
-            if (arr[i] >= left_max) tmp_arr[right_point--] = arr[i];
-            else tmp_arr[left_point++] = arr[i];
-        }
-        for (int i = begin, k = 0; i < end; ++i, ++k) {
-            arr[i] = tmp_arr[k];
-        }
-        //free(tmp_arr);
-        // tmp_arr = (int *) realloc(tmp_arr,sizeof (int));
         Demon_sort(arr, begin, middle);
         Demon_sort(arr, middle, end);
 
@@ -63,25 +76,18 @@ void Demon_sort(int *arr, int begin, int end) {
     } while (right_min < left_max);
 }
 
-
 int main() {
     srand(time(NULL));
-    int len = 10000;
+    int len = 1000;
     int *arr;
     arr = (int *) malloc(len * sizeof(int));
     if (!arr)exit(-1);
     for (int i = 0; i < len; ++i) {
         arr[i] = rand() % 10000;
     }
-    for (int i = 0; i < len; ++i) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
     Demon_sort(arr, 0, len);
-    printf("\n");
-    for (int i = 0; i < len; ++i) {
-        printf("%d ", arr[i]);
-    }
+
+    printf("\nTIME: %li\n", clock());
     free(arr);
     return 0;
 }
